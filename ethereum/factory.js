f78ee769f5b712d329e53bf305f73713878482fa
= <=>
2023-03-24 12:02:01 -0300

    Project updated to v0.8.9

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -2,8 +2,8 @@ import web3 from "./web3";
 import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
-    JSON.parse(CampaignFactory.interface),
-    '0x271e25010fBCB47bB219637756B96f0070AA1B01'
+    CampaignFactory.abi,
+    '0xF12842396b72782bA38de677D39Cccb6FE390560'
 );
 
 export default instance;
